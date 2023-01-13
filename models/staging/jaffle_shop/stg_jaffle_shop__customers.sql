4c722b041e2134d4f86a431868e6da6721f518ba
Thales Donizeti <engmatdoni@gmail.com>
2023-01-13 00:48:12 +0000

    little fix

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -10,7 +10,7 @@ customers as (
     select
         id as customer_id,
         last_name as surname,
-        first_name as givenname,,
+        first_name as givenname,
         first_name || ' ' || last_name as full_name
     from base_customers
 )
