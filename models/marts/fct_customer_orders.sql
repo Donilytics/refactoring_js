39b7979b2ee5013e715ea3b901ce40359b9f5d2d
Thales Donizeti <engmatdoni@gmail.com>
2023-01-13 15:01:00 +0000

    intermediate model add

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,10 +1,10 @@
 with
 
---Import CTEs
 orders as (
 
-    select * from {{ ref("stg_jaffle_shop__orders") }}
-
+    select
+        *
+    from {{ ref('int_orders') }}
 ),
 
 customers as (
@@ -13,14 +13,6 @@ customers as (
 
 ),
 
-payments as (
-
-    select * from {{ ref("stg_stripe__payments") }}
-
-),
-
---Marts
-
 customer_order_history as (
 
     select
@@ -28,43 +20,29 @@ customer_order_history as (
         customers.full_name,
         customers.surname,
         customers.givenname,
-        min(order_date) as first_order_date,
-        min(case
-            when orders.order_status not in ('returned', 'return_pending')
-                then order_date end) as first_non_returned_order_date,
-        max(
-            case
-                when
-                    orders.order_status not in (
-                        'returned', 'return_pending'
-                    ) then order_date
-            end
-        ) as most_recent_non_returned_order_date,
+        min(orders.order_date) as first_order_date,
+        min(orders.valid_order_date) as first_non_returned_order_date,
+        max(orders.valid_order_date) as most_recent_non_returned_order_date,
         coalesce(max(user_order_seq), 0) as order_count,
         coalesce(
-            count(case when orders.order_status != 'returned' then 1 end), 0
+            count(case when orders.valid_order_date is not null then 1 end), 0
         ) as non_returned_order_count,
         sum(
             case
-                when
-                    orders.order_status not in (
-                        'returned', 'return_pending'
-                    ) then payments.payment_amount
-                else 0
+                when orders.valid_order_date is not null
+                then orders.order_value_dollars else 0
             end
         ) as total_lifetime_value,
         sum(
             case
-                when
-                    orders.order_status not in (
-                        'returned', 'return_pending'
-                    ) then payments.payment_amount
-                else 0
+                when orders.valid_order_date is not null
+                then orders.order_value_dollars else 0
             end
         ) / nullif(
             count(
                 case
-                    when orders.order_status not in ('returned', 'return_pending') then 1
+                    when orders.order_value_dollars 
+                    then 1
                 end
             ),
             0
@@ -75,11 +53,6 @@ customer_order_history as (
 
     inner join customers
         on orders.customer_id = customers.customer_id
-
-    left outer join payments
-        on orders.order_id = payments.order_id
-
-    where orders.order_status not in ('pending') and payments.payment_status != 'fail'
 
     group by customers.customer_id, customers.full_name, customers.surname, customers.givenname
 
@@ -97,8 +70,8 @@ final as (
         order_count,
         total_lifetime_value,
         orders.order_status,
-        payments.payment_status,
-        payments.payment_amount as order_value_dollars
+        orders.payment_status,
+        orders.order_value_dollars
     from orders
 
     inner join customers
@@ -107,10 +80,6 @@ final as (
     inner join customer_order_history
         on orders.customer_id = customer_order_history.customer_id
 
-    left outer join payments
-        on orders.order_id = payments.order_id
-
-    where payments.payment_status != 'fail'
 )
 
 --Simple Select Statement
